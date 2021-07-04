758c4313479e2954290d4067dbd97a1d6d281635
Kyle Ortiz <kyleaortiz@gmail.com>
2021-07-04 00:42:20 -0500

    commit tree 421687378dc012133018319c44e28b11127ed92e

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
-
-const titleCased = () => {
-  return tutorials
+let finalArray = [];
+function titleCased() {
+  for (const element of tutorials) {
+    let testArray = [];
+    let index = 0;
+    const sepArray = element.split(" ");
+    const mappedChar = sepArray.map((word) => {
+      const upperCaseChar = word.charAt(0).toUpperCase();
+      testArray.push(upperCaseChar + sepArray[index].substr(1));
+      index = index + 1;
+    });
+    const sentencesForArray = testArray.join(" ");
+    finalArray.push(sentencesForArray);
+  }
+  return finalArray;
 }
